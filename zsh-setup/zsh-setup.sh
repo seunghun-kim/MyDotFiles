52ace8f86353d171ca97e91047d18a0aeede9a96
Seunghun Kim <shkim@info-works.co.kr>
2020-07-31 15:54:50 +0900

    Add zsh setup script

---
+++ zsh-setup/zsh-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Install zsh and oh-my-zsh
+sudo apt install -y zsh curl git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install themes and plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy .zshrc to home directory
+cp ./.zshrc ~
+
+# Install Meslo nerd font series
+mkdir ~/.fonts
+cp ./Meslo/*.ttf ~/.fonts
+
+echo 'Set terminal font to Meslo NF, then run p10k configure'
